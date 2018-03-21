758dee8248e28eee0133e1411cceb797f1649528
Jackamac-attack <zhangjack27@gmail.com>
2018-03-20 21:02:31 -0400

    initial commit

---
+++ ZEB.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pyfolio as pf
+
+
+prices = pd.read_csv('ZEB Historical Data.csv')
+benchmark = pd.read_csv('SP500 Historical Data.csv')
+
+prices = prices[::-1]
+benchmark = benchmark[::-1]
+
+prices['Date'] = pd.to_datetime(prices.Date)
+prices.set_index('Date', inplace=True)
+
+benchmark['Date'] = pd.to_datetime(benchmark.Date)
+benchmark.set_index('Date', inplace=True)
+
+change = prices['Change %'].div(100)
+bench_change = benchmark['Change %'].div(100)
+
+change.dropna(inplace=True)
+bench_change.dropna(inplace=True)
+print(change)
+
+print(change.shape)
+print(bench_change.shape)
+
+# notice to user! rolling fama french has been disabled!!!!!
+pf.tears.create_returns_tear_sheet(change, benchmark_rets=bench_change)
